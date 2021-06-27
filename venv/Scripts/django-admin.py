1db04bf9a41074a932805a708e3e8ef1de3e9987
geeky8 <65672285+geeky8@users.noreply.github.com>
2021-06-27 22:29:38 +0530

    commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\badve\flaskprojects\django-ecommerce-2\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
